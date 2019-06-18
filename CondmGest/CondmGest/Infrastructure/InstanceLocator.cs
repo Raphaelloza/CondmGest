af0a4dae90df8ce2f55497596f56ff8c5d5c6014
Raphaelloza <raphael.loza@hotmail.com>
2019-06-18 12:54:51 -0400

    Login

---
+++ CondmGest/CondmGest/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CondmGest.Infrastructure
+{
+    using ViewModels;
+    
+    class InstanceLocator
+    {
+        //Ligar la MainPage con mi MainPageView
+        #region Properties
+        public MainViewModel Main //Prop
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator() //Ctor
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+
+    }
+}
---
+++ CondmGest/CondmGest/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//La MainViewModel encargada de administradar las demas ViewModels
+
+namespace CondmGest.ViewModels
+{
+    class MainViewModel
+    {
+        #region ViewModels
+
+            public LoginViewModel Login
+            {
+                get;
+                set;
+            }
+
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+
+        #endregion
+    }
+}
